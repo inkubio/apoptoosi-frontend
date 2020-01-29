ad193eeba3af049cfa8c6c79f8ac6ee4a9a7cf0a
Oukki <osiljamo@gmail.com>
2020-01-29 14:14:41 +0200

    Create base for sign up page

---
+++ src/pages/signup/signup.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SignUp = ({}) => {
+
+    const onSubmit = (data) => console.log(data);
+    return <div className={"Base"}>
+        <form onSubmit={onSubmit}>
+            <label>
+                Name:
+                <input type="text" value={} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+    </div>
+};
+
+fun
+export default SignUp;
