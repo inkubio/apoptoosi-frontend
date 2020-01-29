06c291a22273e18012b130aa3363a0c3415b2c43
Oukki <osiljamo@gmail.com>
2020-01-29 23:32:47 +0200

    Create custom hook for sign up

---
+++ src/pages/signup/signUpHook.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+
+const useSignUpForm = (callback) => {
+    const [inputs, setInputs] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        allergies: "",
+        alcohol: "",
+        tableGroup: "",
+        avec: "",
+    });
+
+    const handleSubmit = (event) => {
+        if (event) {
+            event.preventDefault();
+        }
+        callback();
+    };
+
+    const handleInputChange = (event) => {
+        event.persist();
+        setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
+    };
+
+    return {
+        handleSubmit,
+        handleInputChange,
+        inputs
+    };
+}
+
+export default useSignUpForm;
