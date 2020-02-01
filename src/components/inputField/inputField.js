f4bd293dbab43a9aa8ceda642a228989d8681f07
Oukki <osiljamo@gmail.com>
2020-02-01 13:39:38 +0200

    Create input field component

---
+++ src/components/inputField/inputField.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './inputField.css';
+
+const InputField = ({className, type, required, name, placeholder, value, onChange, inputClass, children}) => {
+    return <div className={className}>
+        {required ?
+            <label className={"required"}>{children}</label> :
+            <label>{children}</label>}
+        <input
+            className={inputClass}
+            type={type}
+            name={name}
+            onChange={onChange}
+            placeholder={placeholder || children}
+            value={value}
+            required={required}/>
+    </div>
+};
+
+InputField.propTypes = {
+    className: PropTypes.string,
+    inputClass: PropTypes.string,
+    type: PropTypes.string,
+    required: PropTypes.bool,
+    name: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node])
+};
+
+InputField.defaultProps = {
+    required: false,
+    type: "text"
+};
+
+export default InputField;
